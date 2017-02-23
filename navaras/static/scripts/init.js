9bc2be801fa6225ab7f773323579b5d7e982503a
nagen-mac <nagen@mac.com>
2017-02-22 22:38:32 -0500

    Incorporated all navaras related layout changes, js, css and db removed bootstrap library

---
+++ navaras/static/scripts/init.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+    $('.collapse').sideNav();
+    $('.parallax').parallax();
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    //$('.modal-trigger').leanModal();
+  });
