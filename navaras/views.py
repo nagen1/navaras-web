5017d562d3e94aa25f30f920b5373c72c19dc761
nagen-mac <nagen@mac.com>
2017-02-22 22:19:26 -0500

    project folder name change to navaras and other dependent files with project name in import

--- navaras/views.py
+++ navaras/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from FlaskWebProject1 import app
+from navaras import app
 
 @app.route('/')
 @app.route('/home')
