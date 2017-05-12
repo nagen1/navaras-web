2de6dee85b79c16036eef75348f0ae9eb4407e6e
nagen-mac <nagen@mac.com>
2017-05-11 23:26:15 -0400

    modified:   navaras/__init__.py

--- navaras/__init__.py
+++ navaras/__init__.py
@@ -4,7 +4,7 @@ The flask application package.
 
 from flask import Flask
 app = Flask(__name__)
-app.debug = True
+#app.debug = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 import navaras.views
